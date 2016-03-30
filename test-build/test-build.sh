b0401a0c95270e1f95daa0e6231455f85f2deb05
Harrison-hzp0139 <zhipeng.hui@cloudsoar.com>
2016-03-30 21:29:05 +0800

    this is 11!

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -14,6 +14,6 @@ sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.
 #将占位符赋值为5000，替换为5000加上变量BUILD_NUMBER的值。
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 4C4C52BBC5EED9920183  --secret-key qhQNJU9xwhvJu5duysqccbzMgLZT96Fdo4EmYrrK -p python-redis-demo-build${BUILD_NUMBER} up –d
+./rancher-compose --access-key 4C4C52BBC5EED9920183  --secret-key qhQNJU9xwhvJu5duysqccbzMgLZT96Fdo4EmYrrK -p python-redis-demo-build${BUILD_NUMBER} up -d
 #运行Rancher-compose命令创建一个Stack，名称为python-redis-demo-build加变量{BUILD_NUMBER}的值。添加我们刚刚复制的access key和secret key。
 
